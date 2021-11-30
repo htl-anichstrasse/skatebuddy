4474812f649a1eec5b7bd1da0dffea65e9af72bb
Philipp Schuler <schulerp03@gmail.com>
2021-11-30 20:36:12 +0100

    Initialized project with React Native CLI

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default App;
