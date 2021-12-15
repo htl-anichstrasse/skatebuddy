e651a81e35f51ee20dcae502aa7cd38ea126b8ab
Philipp Schuler <schulerp03@gmail.com>
2021-12-15 20:51:34 +0100

    Added Formik forms

--- mobile/components/Skateparks/SkateparkDetails/AddReviewModal.js
+++ mobile/components/Skateparks/SkateparkDetails/AddReviewModal.js
@@ -1,26 +1,78 @@
 import React, { useState } from 'react';
-import { View, Text, Modal, Pressable } from 'react-native';
+import {
+  View,
+  Text,
+  Modal,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+
+import { Formik } from 'formik';
+
 import styles from '../../../styles/SkateparkDetailsStyles';
 
 const AddReviewModal = ({ modalVisible, setModalVisible }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState('0');
 
   return (
     <Modal
+      hardwareAccelerated={true}
       animationType="slide"
       transparent={true}
       visible={modalVisible}
       onRequestClose={() => {
         setModalVisible(!modalVisible);
       }}>
-      <View style={styles.modalContainer}>
-        <Text>New Review</Text>
-        <Pressable onPress={() => setModalVisible(!modalVisible)}>
-          <Text>Hide Modal</Text>
-        </Pressable>
-      </View>
+      <ScrollView>
+        <View style={styles.modalContainer}>
+          <Text>New Review</Text>
+
+          <Formik
+            initialValues={{ title, content, rating }}
+            onSubmit={values => {
+              console.log(values);
+            }}>
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <>
+                <Text>Title</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('title')}
+                  onBlur={handleBlur('title')}
+                  value={values.title}
+                />
+                <Text>Content</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('content')}
+                  onBlur={handleBlur('content')}
+                  value={values.content}
+                  multiline={true}
+                  numberOfLines={4}
+                />
+                <Text>Rating</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('rating')}
+                  onBlur={handleBlur('rating')}
+                  value={values.rating}
+                  keyboardType="numeric"
+                />
+                <Pressable onPress={handleSubmit}>
+                  <Text>Submit</Text>
+                </Pressable>
+              </>
+            )}
+          </Formik>
+
+          <Pressable onPress={() => setModalVisible(!modalVisible)}>
+            <Text>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </Modal>
   );
 };
--- mobile/styles/SkateparkDetailsStyles.js
+++ mobile/styles/SkateparkDetailsStyles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 const SkateparkDetailsStyles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   title: {
     padding: 10,
     textAlign: 'center',
@@ -20,12 +23,20 @@ const SkateparkDetailsStyles = StyleSheet.create({
   modalContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    height: '80%',
     backgroundColor: '#fff',
+    padding: 20,
     margin: 20,
-    borderColor: '#ddd',
-    borderWidth: 2,
+    borderColor: '#000',
+    borderWidth: 1,
     borderStyle: 'solid',
+    borderRadius: 20,
+  },
+  input: {
+    width: '100%',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    marginBottom: 10,
   },
 });
 
