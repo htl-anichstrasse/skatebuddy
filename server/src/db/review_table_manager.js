739bcaffe7baaf45978c0e0e5a939e6d619827e0
NoF4ll <neunermaxi@gmail.com>
2021-12-20 10:57:00 +0100

    server: added review table manager

---
+++ server/src/db/review_table_manager.js
@@ -0,0 +1,88 @@
+class Review {
+    constructor(parkId, userId, rating, title, content) {
+        this.parkId = parkId;
+        this.userId = userId;
+        this.rating = rating;
+        this.title = title;
+        this.content = content;
+    }
+}
+
+Review.selectAll = con => {
+    return new Promise((resolve, reject) => {
+        con.query('Select * from reviews', (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    });
+};
+
+Review.getById = (con, id) => {
+    return new Promise((resolve, reject) => {
+        con.query(
+            'Select * from reviews where reviewId = ?',
+            [id],
+            (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(result[0]);
+            },
+        );
+    });
+};
+
+Review.insertValue = (con, review) => {
+    return new Promise((resolve, reject) => {
+        con.query(
+            'Insert into reviews(Skateparkid, UserId, Rating, Title, Content) values (?, ?, ?, ?, ?)',
+            [
+                review.parkId,
+                review.userId,
+                review.rating,
+                review.title,
+                review.content,
+            ],
+            (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(result[0]);
+            },
+        );
+    });
+};
+
+Review.update = (con, column, newValue, id) => {
+    return new Promise((resolve, reject) => {
+        con.query(
+            `UPDATE reviews SET ${column} = ? Where reviewId = ? `,
+            [newValue, parseInt(id)],
+            (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result.affectedRows);
+            },
+        );
+    });
+};
+
+Review.deleteValue = (con, id) => {
+    return new Promise((resolve, reject) => {
+        con.query(
+            'Delete from review where ReviewId = ? ',
+            [id],
+            (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(result.affectedRows);
+            },
+        );
+    });
+};
+
+module.exports = Review;
