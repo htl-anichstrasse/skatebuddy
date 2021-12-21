7d08893115af9eca74f500f31e8d7f2cd3007e4a
NoF4ll <neunermaxi@gmail.com>
2021-12-21 14:40:56 +0100

    server: finnished database table routes

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const userRouter = require('./routes/user_routes');
 const obstacleRouter = require('./routes/obstacle_routes');
 const skateparkRouter = require('./routes/skatepark_routes');
 const skateparkPicRouter = require('./routes/skatepark_picture_routes');
+const reviewRouter = require('./routes/review_routes');
+const skateparkObstacleRouter = require('./routes/skatepark_obstacle_con_routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
@@ -14,6 +16,8 @@ app.use('/api', userRouter);
 app.use('/api', skateparkRouter);
 app.use('/api', obstacleRouter);
 app.use('/api', skateparkPicRouter);
+app.use('/api', reviewRouter);
+app.use('/api', skateparkObstacleRouter);
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
