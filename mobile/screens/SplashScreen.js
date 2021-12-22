342405285d8d772f78c6efbd5e6bb3cd330ef0cd
Philipp Schuler <schulerp03@gmail.com>
2021-12-22 09:19:27 +0100

    added basic splashscreen, loginscreen, signupscreen

---
+++ mobile/screens/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../styles/GlobalStyles';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.splashScreen}>
+      <Text style={styles.splashScrenText}>🛹</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
