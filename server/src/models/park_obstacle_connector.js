657c2330d57ab052b5ab34e2ddfd5a2309408d49
NoF4ll <55537941+NoF4ll@users.noreply.github.com>
2022-01-09 17:01:45 +0100

    server: deleted useless classes

--- server/src/models/park_obstacle_connector.js
+++
@@ -1,8 +0,0 @@
-class ParkObstacleConnector {
-    constructor(obstacleId, parkId) {
-        this.obstacleId = obstacleId;
-        this.parkId = parkId;
-    }
-}
-
-module.exports = ParkObstacleConnector;
--- server/src/server.js
+++ server/src/server.js
@@ -10,24 +10,19 @@ const obstacleRouter = require('./routes/obstacle_routes');
 const skateparkRouter = require('./routes/skatepark_routes');
 const skateparkPicRouter = require('./routes/skatepark_picture_routes');
 const reviewRouter = require('./routes/review_routes');
-const skateparkObstacleRouter = require('./routes/skatepark_obstacle_con_routes');
-//const loginRouter = require('./routes/util/login_routes');
-//(const signUpRouter = require('./routes/util/sign_up_routes');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use('/api', userRouter);
-app.use('/api', skateparkRouter);
-app.use('/api', obstacleRouter);
-app.use('/api', skateparkPicRouter);
-app.use('/api', reviewRouter);
-app.use('/api', skateparkObstacleRouter);
-/*app.use('/api', signUpRouter);
-app.use('/api', loginRouter);
-*/
+app.use(bodyParser.json()),
+    use('/api', userRouter),
+    use('/api', skateparkRouter),
+    use('/api', obstacleRouter),
+    use('/api', skateparkPicRouter),
+    use('/api', reviewRouter);
+
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 });
