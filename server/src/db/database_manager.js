adf0460d3a4bccd8d475ee2eeb471fa0cad8bbb1
NoF4ll <neunermaxi@gmail.com>
2022-01-12 23:23:47 +0100

    server: database secrets and workflows

--- server/src/db/database_manager.js
+++ server/src/db/database_manager.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 const con = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: '3306',
-    database: 'skater_app',
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
 });
 
 module.exports = con;
