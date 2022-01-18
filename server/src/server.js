a311208eb6d83aa1f731e5832a0312750df748ec
NoF4ll <neunermaxi@gmail.com>
2022-01-18 21:15:41 +0100

    server: bugfixing alex website shit

--- server/src/server.js
+++ server/src/server.js
@@ -33,11 +33,14 @@ app.use('/api', skateparkPicRouter);
 app.use('/api', reviewRouter);
 app.use(cors());
 
-app.use(() => (req, res, next) {
-    res.header('Access-Controll-Allow-Origin': '*')
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
+app.use((req, res, next) => {
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+});
 
 app.listen(port, () => {
     console.log(`app listening at https://skate-buddy.josholaus.com/api/skateparks
