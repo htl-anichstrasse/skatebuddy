1a4b193f28138434c5cabdfb2df70c1c75ac45f2
Philipp Schuler <schulerp03@gmail.com>
2022-02-03 11:35:57 +0100

    userdata fetch completed

--- mobile/components/SkateparksList/Entry/DirectionsMethod.js
+++ mobile/components/SkateparksList/Entry/DirectionsMethod.js
@@ -1,5 +1,5 @@
 // libraries
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
@@ -10,13 +10,16 @@ import Text from '../../common/Text';
 import styles from '../../../styles/SkateparksStyles';
 
 const DirectionsMethod = ({ icon, duration, color }) => {
-  if (duration.value > 5400) {
+  if (duration && duration.value > 5400) {
     duration.text = '> 1,5h';
   }
+
   return (
     <View style={styles.entryDirectionsMethodContainer}>
       {/* //* duration.value seconds / duration.text human readable*/}
-      <Text style={styles.entryDirectionsMethodText}>{duration.text}</Text>
+      <Text style={styles.entryDirectionsMethodText}>
+        {duration && duration.text}
+      </Text>
       <FontAwesome5 name={icon} size={20} color={color} />
     </View>
   );
