fd842848444e28f1a476585855b824ad17730c05
Alexander Bertoni <bertonialex63@gmail.com>
2022-02-07 16:43:18 +0100

    Reviews show more button

--- website/src/reviews/ReviewList.js
+++ website/src/reviews/ReviewList.js
@@ -1,17 +1,23 @@
 import './ReviewList.css';
+import ShowMore from 'react-show-more-button';
+
+
+const button = <button className='show-more-button'>Show more</button>
 
 const ReviewList = ( {reviews} ) => {
   return (
     <div className="review-list">
       {reviews.map(review => (
-        <div className="review-content" key={review.skateparkId}>
+          <div className="review-content" key={review.skateparkId}>
           <div className="reviews-in-box">
             <h4 className='h4-rating'>{review.reviewId}  {review.title}</h4>
             <p>{review.username}</p>
             <div id="rating" className="rating">
               <h5 className='h5-rating'>Bewertung: {review.rating}</h5>
             </div><br/>
+            <ShowMore maxHeight={190} button={button}>
             <p className='bottom-text'>{review.content}</p>
+            </ShowMore>
           </div>
         </div>
       ))}
