edde377b561b1c596ad9a77547e560ea66f34d82
Philipp Schuler <schulerp03@gmail.com>
2022-02-21 20:54:01 +0100

    added image from server to skateparkslist

--- mobile/components/SkateparksList/Entry/SkateparkEntry.js
+++ mobile/components/SkateparksList/Entry/SkateparkEntry.js
@@ -1,6 +1,6 @@
 // librarys
 import React from 'react';
-import { View, Pressable, Image } from 'react-native';
+import { View, Pressable, Image, Dimensions } from 'react-native';
 
 // components
 import EntryHeader from './EntryHeader';
@@ -11,7 +11,6 @@ import EntryDirections from './EntryDirections';
 // styles
 import styles from '../../../styles/SkateparksStyles';
 import gStyles from '../../../styles/GlobalStyles';
-import { skateparksImages } from '../../../styles/Images';
 
 const SkateparkEntry = ({
   skatepark,
@@ -19,30 +18,56 @@ const SkateparkEntry = ({
   location,
   locLoading,
   locError,
-}) => (
-  <Pressable
-    onPress={() => {
-      navigation.navigate('SkateparkDetails', {
-        skatepark,
-        scroll: null,
-      });
-    }}
-  >
-    <View style={[styles.entryContainer, gStyles.shadow]}>
-      <EntryHeader skatepark={skatepark} navigation={navigation} />
-      <EntryDirections
-        skatepark={skatepark}
-        location={location}
-        locLoading={locLoading}
-        locError={locError}
-      />
+}) => {
+  let url = 'https://skate-buddy.josholaus.com/api/skateparkpictures/';
+  url += skatepark.pictureIds[0].skateparkId;
+  url += '/';
+  url += skatepark.pictureIds[0].picId;
 
+  return (
+    <Pressable
+      onPress={() => {
+        navigation.navigate('SkateparkDetails', {
+          skatepark,
+          scroll: null,
+        });
+      }}
+    >
+      <View style={[styles.entryContainer, gStyles.shadow]}>
+        <EntryHeader skatepark={skatepark} navigation={navigation} />
+        <EntryDirections
+          skatepark={skatepark}
+          location={location}
+          locLoading={locLoading}
+          locError={locError}
+        />
+
+        {/* 
+
+    return (
       <Image
-        style={styles.entryImage}
-        source={skateparksImages[skatepark.skateparkId].main}
+        source={{
+          uri: url,
+          width: Dimensions.get('window').width - 80,
+          height: 200,
+        }}
+        style={{
+          flex: 1,
+        }}
       />
-    </View>
-  </Pressable>
-);
+    );
+  }; */}
+        <Image
+          source={{
+            uri: url,
+            width: Dimensions.get('window').width - 40,
+            height: (Dimensions.get('window').width - 40) * (9 / 16),
+          }}
+          style={styles.entryImage}
+        />
+      </View>
+    </Pressable>
+  );
+};
 
 export default SkateparkEntry;
