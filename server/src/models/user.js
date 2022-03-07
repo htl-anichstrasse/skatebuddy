43884f50b06d0700e105a777e38f40da28605e67
NoF4ll <55537941+NoF4ll@users.noreply.github.com>
2022-03-07 18:46:32 +0100

    server: added boolean admin or not

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,10 +1,11 @@
 class User {
-    constructor(userId, name, passwordhash, email, profilepictureId) {
+    constructor(userId, name, passwordhash, email, profilepictureId, admin) {
         this.userId = userId;
         this.name = name;
         this.passwordhash = passwordhash;
         this.email = email;
         this.profilepictureId = profilepictureId;
+        this.admin = admin;
     }
 }
 
